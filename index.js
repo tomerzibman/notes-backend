d18efcd5e8cfeee1a43b4f348f19eb1d7cd66245
tomerzibman <tomerzibman@gmail.com>
2024-01-21 14:34:02 -0500

    added eslint and adjusted for rules

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const Note = require('./models/note');
 
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(cors());
 
 
-const unknownEndpoint = (req, res, next) => {
+const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: 'Unknown endpoint' });
-}
+};
 
 const errorHandler = (err, req, res, next) => {
   console.log(err);
@@ -21,20 +21,20 @@ const errorHandler = (err, req, res, next) => {
   } else if (err.name === 'ValidationError') {
     return res.status(400).json({ error: err.message });
   }
-  next(error);
-}
+  next(err);
+};
 
-app.get("/", (req, res, next) => {
-  res.send("<h1>Hello World</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
 });
 
-app.get("/api/notes", (req, res, next) => {
+app.get('/api/notes', (req, res) => {
   Note.find({}).then((notes) => {
     res.json(notes);
   });
 });
 
-app.get("/api/notes/:id", (req, res, next) => {
+app.get('/api/notes/:id', (req, res, next) => {
   Note.findById(req.params.id).then(note => {
     if (note) {
       res.json(note);
@@ -44,11 +44,11 @@ app.get("/api/notes/:id", (req, res, next) => {
   }).catch(err => next(err));
 });
 
-app.post("/api/notes", (req, res, next) => {
+app.post('/api/notes', (req, res, next) => {
   const body = req.body;
   if (!body.content) {
     return res.status(400).json({
-      error: "content missing",
+      error: 'content missing',
     });
   }
   const note = new Note({
@@ -73,8 +73,8 @@ app.put('/api/notes/:id', (req, res, next) => {
   }).catch(err => next(err));
 });
 
-app.delete("/api/notes/:id", (req, res, next) => {
-  Note.findByIdAndDelete(req.params.id).then(result => {
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndDelete(req.params.id).then(() => {
     res.status(204).end();
   }).catch(err => next(err));
 });
