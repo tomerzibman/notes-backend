1f47824d0210ed9611706444e246d809b2433a36
tomerzibman <tomerzibman@gmail.com>
2024-01-25 16:37:05 -0500

    added user authetication with jwt

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('express-async-errors');
 const notesRouter = require('./controllers/notes');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -29,6 +30,7 @@ app.use(cors());
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,15 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFromReq = (req) => {
+  const authorization = req.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
@@ -19,7 +28,11 @@ notesRouter.get('/:id', async (req, res) => {
 notesRouter.post('/', async (req, res) => {
   const body = req.body;
 
-  const user = await User.findById(body.userId);
+  const decodedToken = jwt.verify(getTokenFromReq(req), process.env.SECRET);
+  if (!decodedToken.id) {
+    return res.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
